8a0ef5e7b0c5c2ad459bb689c873631da0b41e3f
arafat-yasin-2413 <yasinarafat2413@gmail.com>
2025-05-03 02:40:41 +0600

    52.5-2 Done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import { NavLink } from "react-router";
+import { Link, NavLink } from "react-router";
 import user from "../assets/user.png";
+
+
+
+
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center">
       <div className=""></div>
       <div className="nav flex gap-5 text-accent">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/career">Career</NavLink>
+        <NavLink className={"p-2"} to="/">Home</NavLink>
+        <NavLink className={"p-2"} to="/about">About</NavLink>
+        <NavLink className={"p-2"} to="/career">Career</NavLink>
       </div>
       <div className="login-btn flex gap-5">
         <img src={user} alt="" />
-        <button className="btn btn-primary px-10 ">Login</button>
+        <Link to="/auth/login" className="btn btn-primary px-10 ">Login</Link>
       </div>
     </div>
   );
---
+++ src/pages/Register.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Register = () => {
+    return (
+        <div className="flex justify-center min-h-screen items-center">
+                    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
+                        
+                        <h2 className="text-center text-2xl font-semibold mt-5">Please Register</h2>
+                        <div className="card-body">
+                            <fieldset className="fieldset">
+                                
+                                {/* name */}
+                                <label className="label">Name</label>
+                                <input
+                                    type="text"
+                                    className="input"
+                                    placeholder="name"
+                                />
+                                
+                                
+                                {/* photo url */}
+                                <label className="label">Photo URL</label>
+                                <input
+                                    type="text"
+                                    className="input"
+                                    placeholder="photo url"
+                                />
+
+
+                                {/* email */}
+                                <label className="label">Email</label>
+                                <input
+                                    type="email"
+                                    className="input"
+                                    placeholder="Email"
+                                />
+                                
+                                {/* password */}
+                                <label className="label">Password</label>
+                                <input
+                                    type="password"
+                                    className="input"
+                                    placeholder="Password"
+                                />
+                                
+                                <button className="btn btn-neutral mt-4">Register</button>
+                                
+                                <p className="text-center text-sm font-semibold pt-5" >Already have an account? <Link to="/auth/login" className="text-secondary ">Login</Link> </p>
+                            </fieldset>
+        
+        
+                        </div>
+                    </div>
+                </div>
+    );
+};
+
+export default Register;
